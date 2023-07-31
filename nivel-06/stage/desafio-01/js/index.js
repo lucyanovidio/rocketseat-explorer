1dbc8133c95ee453d839b5c4fae2464cd6fd8f42
lucyanovidio <lucyanovidio@gmail.com>
2023-07-31 18:34:41 -0300

    :bug: fix: Corrigindo URLs

--- nivel-06/stage/desafio-01/js/index.js
+++ nivel-06/stage/desafio-01/js/index.js
@@ -2,13 +2,11 @@ import { Router } from "./router.js";
 
 const router = new Router();
 
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-01/", "/pages/home.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-01/about", "/pages/about.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-01/contact", "/pages/contact.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-01/404", "/pages/404.html");
+router.add("/", "/pages/home.html");
+router.add("/about", "/pages/about.html");
+router.add("/contact", "/pages/contact.html");
+router.add("/404", "/pages/404.html");
 
 router.handle();
-
 window.onpopstate = () => router.handle();
-
 window.route = () => router.route();
--- nivel-06/stage/desafio-02/src/js/index.js
+++ nivel-06/stage/desafio-02/src/js/index.js
@@ -2,10 +2,10 @@ import { Router } from "./router.js";
 
 const router = new Router();
 
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-02/", "../../pages/home.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-02/universe", "../../pages/universe.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-02/exploration", "../../pages/exploration.html");
-router.add("/rocketseat-explorer/nivel-06/stage/desafio-02/404", "../../pages/404.html");
+router.add("/", "../../pages/home.html");
+router.add("/universe", "../../pages/universe.html");
+router.add("/exploration", "../../pages/exploration.html");
+router.add(404, "../../pages/404.html");
 
 router.handle();
 window.onpopstate = () => router.handle();
